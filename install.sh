187b058732b9c51b34b08e23cacc50e394138799
Walter Mitty <walter.mitty@tuta.io>
2025-06-25 17:02:59 +0300

    update installation script

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ set -u
 sudo apt update && \
 sudo apt install fzf tmux 
 
-ln -f -s `pwd`/bashrc ~/.bashrc
-ln -f -s `pwd`/gitconfig ~/.gitconfig
-ln -f -s `pwd`/tmux.conf ~/.tmux.conf
+ln -f -s `pwd`/bashrc $HOME/.bashrc
+ln -f -s `pwd`/gitconfig $HOME/.gitconfig
+ln -f -s `pwd`/tmux.conf $HOME/.tmux.conf
+ln -f -s `pwd`/vim $HOME/.vim
+ln -f -s `pwd`/vimrc $HOME/.vimrc
